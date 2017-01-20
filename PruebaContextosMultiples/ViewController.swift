1385d68b0f35f82382888fbd25ca787769147a4f
Otto Colomina Pardo <otto@dccia.ua.es>
2017-01-20 10:59:50 +0100

    Initial Commit

---
+++ PruebaContextosMultiples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaContextosMultiples
+//
+//  Created by Otto Colomina Pardo on 20/1/17.
+//  Copyright © 2017 Universidad de Alicante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
